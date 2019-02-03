34cb5ac00765812fa67a0660189e4499dd919103
Chris Caragianis <chriscaragianis@gmail.com>
2019-02-02 22:27:27 -0500

    first room almost done

---
+++ find_me/Assets/flickerscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flickerscript : MonoBehaviour {
+
+	private Animator _animator;
+	// Use this for initialization
+	void Start () {
+	    _animator = GetComponent<Animator> ();
+		_animator.SetBool ("flicker", false);
+	}
+	
+	void OnTriggerEnter(Collider other) {	
+		Debug.Log ("slkjf");
+		if (other.CompareTag("Player")) {
+			_animator.SetBool ("flicker", true);
+
+		}
+	}
+}
